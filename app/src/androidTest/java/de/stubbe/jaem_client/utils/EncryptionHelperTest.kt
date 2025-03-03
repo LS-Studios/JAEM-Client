5dbca0849dfc51884a69416f78cf6dda41c7de43
ls-studios <stubbelennard135@gmail.com>
2025-03-03 13:15:48 +0100

    generated

---
+++ app/src/androidTest/java/de/stubbe/jaem_client/utils/EncryptionHelperTest.kt
@@ -0,0 +1,54 @@
+package de.stubbe.jaem_client.utils
+
+import org.junit.jupiter.api.Assertions.*
+
+class EncryptionHelperTest {
+
+    @org.junit.jupiter.api.Test
+    fun getAesKey() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun setAesKey() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getOtherClient() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun setOtherClient() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun setCommunicationPartner() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun encrypt() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun decrypt() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun init() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getClient() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun setClient() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getEncryption() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun setEncryption() {
+    }
+}
